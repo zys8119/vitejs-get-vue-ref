b3a74fbab3c258bdadafd3e9e9b75f6016e1f6e7
zys8119 <770959294@qq.com>
2025-06-27 09:43:05 +0800

    [add] update

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,8 @@
 declare global {
-    const __getVueRefvm__: any;
-    function getVueRef(refName: any): any;
+  const __getVueRefvm__: any;
+  function getVueRef(refName: any): any;
 }
 export {};
+import { Plugin } from "vite";
+export declare const getVueRefPlugin: () => Plugin;
+export default getVueRefPlugin;
